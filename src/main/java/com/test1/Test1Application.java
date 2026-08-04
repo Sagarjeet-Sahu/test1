3b339683d0d28b394a8420d4496971edaf652b23
Sagarjeet Sahu <143352568+Sagarjeet-Sahu@users.noreply.github.com>
2026-08-05 02:16:39 +0530

    Somethig add

--- src/main/java/com/test1/Test1Application.java
+++ src/main/java/com/test1/Test1Application.java
@@ -8,6 +8,7 @@ public class Test1Application {
 
 	public static void main(String[] args) {
 		    int y=10;
+                    boolean isSub=true;
 		    SpringApplication.run(Test1Application.class, args);
 	}
 
